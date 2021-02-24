96c76785ca778fbf041e413ccf5f32a2ab2b7aab
Marc Domingo <mdomingo@chapan.edu>
2021-02-24 15:48:59 -0800

    Update GameManagerScript.cs

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿/*
+Name: Marc Domingo
+Student ID: 2346778
+Chapman Email: mdomingo@chapman.edu
+Course Number and Section: 236-03
+Assignment: Project 2
+This is my own work, and I did not cheat on this assignment.
+*/
+
+// The purpose of this project is to display a random student's name from a file on the screen when a button on Unity's UI is pressed.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -10,9 +20,11 @@ public class GameManagerScript : MonoBehaviour
     // On button click in UI, randomly pick a student name
     // Assign to text object in GUI the chosen student name
 
+    // Can be changed later via Unity
     public string fileName = "default.txt";
+    // The display textbox object in Unity
     public Text displayName;
-
+    // The array that will be used to store the names of students from a file
     private string[] studentNames;
 
     // Start is called before the first frame update
@@ -21,15 +33,21 @@ public class GameManagerScript : MonoBehaviour
         LoadStudentNames();
     }
 
+    // Function to be used to fill the array "studentNames" with the names of students from a file. 
     private void LoadStudentNames()
     {
+        // For each line in the opened file, add the name to the array of student names.
         this.studentNames = File.ReadAllLines(fileName);
     }
 
+    // Function that is called whenever the UI button in Unity is pressed.
     public void ButtonClick()
     {
+        // Generate a random number between 0 (inclusive) and the length of the array "studentNames" (exclusive).
         int i = UnityEngine.Random.Range(0, this.studentNames.Length);
+        // Using the randomly generated number, assign the string variable "name" to the student's name at a specific index within the array "studentNames."
         string name = this.studentNames[i];
+        // Display the chosen name onto the textbox in Unity.
         displayName.text = name;
     }
 }
